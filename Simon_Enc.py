083980bedabd76f722970df54a8b69cc78d5257d
Deny Kurniawan <dekakrens@gmail.com>
2021-11-29 23:14:08 +0700

    menambahkan timing

--- Simon_Enc.py
+++ Simon_Enc.py
@@ -3,6 +3,9 @@ from random import randint
 from time import sleep
 from collections import deque
 import paho.mqtt.client as mqtt
+import timeit
+from datetime import datetime
+import json
 
 #MQTT
 mqttBroker = "192.168.8.153"
@@ -217,16 +220,31 @@ class SimonCipher(object):
                 raise
         return self.iv
 
+def pencatatan(i, waktu):
+    f = open('publish.csv', 'a')
+    f.write("Message ke-" + i + ";" +waktu + "\n")
 
-if __name__ == "__main__":
-    while True:
-        key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100
-        cipher = SimonCipher(key, 256, 128, 'CBC', 0xF925)
-        for _ in range(100):
-            mess = int('{:10}'.format(randint (60,100)))
-            print("Pesan yang dikirim\t: ", mess)
-            g = cipher.encrypt(mess)
-            client.publish("SIMON", g)
-            print("Encrypted\t\t: ", hex(g))
-            print("Just published " + hex(g) + " to topic SIMON")
-            sleep(3)
+# Mencatat waktu mulai
+start = timeit.default_timer()
+
+key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100
+cipher = SimonCipher(key, 256, 128, 'CBC', 0xF925)
+message ={}
+for i in range(3):
+    mess = int('{:10}'.format(randint (60,100)))
+    print("Pesan yang dikirim\t: ", mess)
+    g = cipher.encrypt(mess)
+    now = str(datetime.now())
+    # pencatatan(str(i), now)
+    message['cipher'] = g
+    message['datetime'] = now
+    stringify = json.dumps(message, indent=2)
+    client.publish("SIMON", stringify)
+    print("Encrypted\t\t: ", hex(g))
+    print("Just published a message to topic SIMON at "+ now)
+    sleep(1)
+
+# Mencatat waktu selesai
+stop = timeit.default_timer()
+lama_enkripsi = stop - start
+print("Waktu akumuasi : "+str(lama_enkripsi))
