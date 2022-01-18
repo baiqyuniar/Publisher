910c1095970c1cf9e72e06f6b580bd0ce8e417b2
baiqyuniar <baiqyustiarini@gmail.com>
2022-01-18 14:15:37 +0700

    aes128

--- Simon_Enc.py
+++ Simon_Enc.py
@@ -226,11 +226,11 @@ def pencatatan(i, waktu):
 
 # Mencatat waktu mulai
 start = timeit.default_timer()
-#key = 0x1f1e1d1c1b1a19181716151413121110
+key = 0x1f1e1d1c1b1a19181716151413121110
 #key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a0908
-key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100
-#cipher = SimonCipher(key, 256, 128, 'ECB')
-cipher = SimonCipher(key, 256, 128, 'CBC', 0x123456789ABCDEF0)
+#key = 0x1f1e1d1c1b1a191817161514131211100f0e0d0c0b0a09080706050403020100
+cipher = SimonCipher(key, 128, 128, 'ECB')
+#cipher = SimonCipher(key, 256, 128, 'CBC', 0x123456789ABCDEF0)
 message ={}
 for i in range(10000):
     mess = randint (60,100)
